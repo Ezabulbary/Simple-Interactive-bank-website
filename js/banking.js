a47573509a124a1ce579892842cd5e3d24447157
Ezabulbary <ezabul.bary@gmail.com>
2022-02-26 03:51:12 +0600

    js banking

--- js/banking.js
+++ js/banking.js
@@ -23,3 +23,25 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
 
 
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputText = withdrawInput.value;
+    const newWithdrawInput =parseFloat(withdrawInputText);
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const previusWithdrawTotal = parseFloat(withdrawTotalText);
+
+    const totalWithdraw = newWithdrawInput + previusWithdrawTotal;
+    withdrawTotal.innerText = totalWithdraw;
+
+    const blanceTotal = document.getElementById('blance-total');
+    const blanceTotalText = blanceTotal.innerText;
+    const previusBlanceTotal = parseFloat(blanceTotalText);
+
+    const newTotalBlance = previusBlanceTotal - newWithdrawInput;
+
+    blanceTotal.innerText = newTotalBlance;
+    
+    withdrawInput.value = '';
+});
